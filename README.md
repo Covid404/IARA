8e83f03bb44128e438c840cfa7bf7c3f2e3f600d
renan <renancunhafonseca@gmail.com>
2020-06-16 12:03:02 -0300

    Add cleaned data of SP

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Depois a gt acha um nome melhor
 
 ## Fonte dos Dados:
 
+* [SP](https://www.saopaulo.sp.gov.br/coronavirus/transparencia/)
 * [MG](http://www.transparencia.dadosabertos.mg.gov.br/dataset/contratacoes-coronavirus)
 * [ES](https://coronavirus.es.gov.br/contratos-emergenciais)
-* [PA](http://www.transparencia.pr.gov.br/pte/compras/dispensasInexigibilidade?windowId=9f5)
+* [PR](http://www.transparencia.pr.gov.br/pte/compras/dispensasInexigibilidade?windowId=9f5)
 * [RO](http://www.transparencia.ro.gov.br/Grafico/DespesasCOVID19)
---
+++ src/clean_data/clean_data_sp.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+path_file = "data/dados_sp.csv"
+
+
+def clean_data_sp(path_file: str = path_file) -> pd.DataFrame:
+    df = pd.read_csv(path_file, sep=";", encoding = "ISO-8859-1")
+    df['objeto'] = df['Item / Finalidade']
+    
+    ventilador_index = df['objeto'].str.contains("VENTILADOR")
+    aquisition_index = df['Descrição Processo'].str.contains("AQUISIÇÃO DE EQUIP")
+    index = ventilador_index & aquisition_index
+    df = df.loc[index, ["objeto", "Quantidade Item", " Valor Unitário ",
+                        "Data Emissão do Pagamento"]]
+    print(df)
+    df.columns = ["nome", "quantidade", "preco", "data"]
+    df['estado'] = ['SP']*df.shape[0]
+    return df
+
+
+
+if __name__ == "__main__":
+    print(clean_data_sp())

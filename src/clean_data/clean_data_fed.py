31c50bc4b578a67a66691577e7af3cc0e10b8456
rjlmota <rjlmota@gmail.com>
2020-06-20 15:26:03 -0300

    added id to fed data

--- src/clean_data/clean_data_fed.py
+++ src/clean_data/clean_data_fed.py
@@ -8,8 +8,8 @@ def clean_data_fed(path_file: str = path_file) -> pd.DataFrame:
     df = pd.read_csv(path_file)
     df_ventiladores = df[df['ITEM_DESCRICAO'].str.contains('VENTILADOR ART')]
     df_ventiladores = df_ventiladores[~df_ventiladores['ITEM_DESCRICAO'].str.contains('CIRCUITO|MOTOR')]
-    df_ventiladores = df_ventiladores[['ITEM_DESCRICAO', 'PRECO_UNITARIO', 'DATA','UF', 'QUANTIDADE']]
-    df_ventiladores.columns = ['nome', 'preco', 'data', 'estado', 'quantidade']
+    df_ventiladores = df_ventiladores[['COD_COMPRA', 'ITEM_DESCRICAO', 'PRECO_UNITARIO', 'DATA','UF', 'QUANTIDADE']]
+    df_ventiladores.columns = ['id', 'nome', 'preco', 'data', 'estado', 'quantidade']
 
     return df_ventiladores
 
